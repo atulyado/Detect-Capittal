ff5247db0998188d9bd90b42a6331bbdb610cb92
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-17 22:05:53 +0700

    Update detectCapital.java

--- detectCapital.java
+++ detectCapital.java
@@ -2,6 +2,6 @@
 import java.util.regex.Pattern;
 class Solution {
     public boolean detectCapitalUse(String word) {
-       return Pattern.matches("([A-Z]+|[a-z]+|[A-Z][a-z]*)",word); 
-    }
+       return Pattern.matches("([A-Z]+|[a-z]+|[A-Z][a-z]*)",word);//[A-Z] checks all caps [a-z] checks all lows [A-Z][a-z] check if the first is caps and last is low
+    }// example this FLaG this is false
 }
